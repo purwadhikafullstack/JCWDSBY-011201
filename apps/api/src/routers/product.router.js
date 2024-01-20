8dd6d1d75435faa427e03f5931fc8f8aa26157ea
fahmi997 <aardiansyah.fahmii@gmail.com>
2024-01-21 04:03:08 +0700

    fix: remove unused function in product.router

--- apps/api/src/routers/product.router.js
+++ apps/api/src/routers/product.router.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
 import {
-    // getProductData,
-    // getInventoryData,
-    // createProduct,
-    // updateProduct,
-    updateInventory, 
     deleteProduct,
     deleteInventory,
-    // findProductByName,
     findProductByCategory,
 } from "../controllers/product.controller";
 import getAllProduct from "./product/getAllProduct";
-import getAllInventory from "./inventory/getAllInventory";
 import createProduct from "./product/createProduct";
 import findProductByName from "./product/findProductByName";
 import updateProduct from "./product/updateProduct";
@@ -20,7 +13,6 @@ import { validateSuper, validateToken } from "../middleware/tokenValidation";
 const productRouter = Router();
 
 productRouter.get('/', getAllProduct);
-// productRouter.get('/inventory', getAllInventory);
 productRouter.get('/:name', findProductByName);
 productRouter.post('/', validateToken, validateSuper, createProduct);
 productRouter.patch('/:id', validateToken, validateSuper, updateProduct);
@@ -33,15 +25,6 @@ productRouter.get('/category/:name', async (req, res, next) => {
         next(error);
     }
 });
-
-// productRouter.patch('/inventory/:id', async (req, res, next) => {
-//     try {
-//         await updateInventory(req.params.id, req.body);
-//         res.status(200).json('Inventory updated');
-//     } catch (error) {
-//         next(error)
-//     }
-// });
 
 productRouter.delete('/:id', async (req, res, next) => {
     try {
