434e0732a4638d3e0cf592c418f4ba950cf25266
TableStar <103476393+TableStar@users.noreply.github.com>
2024-02-08 01:08:08 +0700

    Transaction stock (#29)
    * feat: forgot password and reset password
    
    * feat: cart front
    
    * fix: temp disable cypress
    
    * fix: change instance of productId in cart to inventoryId
    
    * fix: change instance of productId in cart to inventoryId
    
    * feat: cart redux
    
    * feat: delete one item in cart api
    
    * feat: connecting cart and inventory w/ sqlize assosiaction
    
    * chore: npm install
    
    * fix: app.jsx comment
    
    * feat: add resi column to transactions table
    
    * feat: add shipmentName column to transactions
    
    * fix: stock and bookedstock change during transaction
    
    ---------
    
    Co-authored-by: wahyuwidiantoro83 <wahyuwidiantoro83@gmail.com>

--- apps/web/src/helpers/checkout/updateTransaction.js
+++ apps/web/src/helpers/checkout/updateTransaction.js
@@ -1,17 +1,36 @@
+import customToast from '../../utils/toast';
 import API_CALL from '../API';
 
 export const updateTransactionStatus = async (invoice, status) => {
   try {
     const response = await API_CALL.patch(
       `/transaction/${invoice}`,
-      { status:status },
+      { status: status },
       {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('authToken')}`,
         },
       },
     );
-    console.log("🚀 ~ updateTransactionStatus ~ response:", response)
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const handleSuccessCheckout = async (invoice, status) => {
+  try {
+    const response = await API_CALL.patch(
+      `/transaction/success/${invoice}`,
+      { status },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+        },
+      },
+    );
+    if (response) {
+      customToast('success', response.data.message);
+    }
   } catch (error) {
     console.log(error);
   }
