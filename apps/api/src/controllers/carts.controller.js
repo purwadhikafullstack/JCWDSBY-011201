e867fe57ba336d6c8a7c36e62d850dc7979b6982
TableStar <afraroad@gmail.com>
2024-02-11 20:12:59 +0700

    fix: finish migrating cart controller and service

--- apps/api/src/controllers/carts.controller.js
+++ apps/api/src/controllers/carts.controller.js
@@ -1,11 +1,6 @@
-import carts from '../models/carts.model';
-import inventory from '../models/inventory.model';
-import product from '../models/product.model';
-import productImage from '../models/product-image.model';
-import stores from '../models/stores.model';
-import discount from '../models/discount.model';
 import {
   addCarts,
+  deleteCheckedItemInCart,
   deleteOneProductInCart,
   findOneCartById,
   findOneCartByInventoryId,
@@ -129,131 +124,33 @@ export const updateCartController = async (req, res, next) => {
   }
 };
 
-// export const addCarts = async (req, res, t) => {
-//   return await carts.create(
-//     { ...req.body, userId: req.tokenData.id },
-//     { transaction: t },
-//   );
-// };
+export const deleteCheckedItemController = async (req, res, next) => {
+  await DB.initialize();
+  try {
+    await DB.db.sequelize.transaction(async (t) => {
+      await deleteCheckedItemInCart(req, t);
+    });
+    return res
+      .status(200)
+      .json({ success: true, message: 'checked items deleted successfully' });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
-// export const incrementCartAmountBy1 = async (req, t) => {
-//   return await carts.increment('amount', {
-//     where: { userId: req.tokenData.id, inventoryId: req.body.inventoryId },
-//     transaction: t,
-//   });
-// };
-// export const findOneCartByInventoryId = async (req) => {
-//   return await carts.findOne({
-//     where: { userId: req.tokenData.id, inventoryId: req.body.inventoryId },
-//   });
-// };
-// export const findOneCartById = async (req) => {
-//   return await carts.findOne({ where: { id: req.params.id }, raw: true });
-// };
+export const deleteOneItemController = async (req, res, next) => {
+  await DB.initialize();
+  try {
+    await DB.db.sequelize.transaction(async (t) => {
+      await deleteOneProductInCart(req, t);
+    });
+    return res
+      .status(200)
+      .json({ success: true, message: 'Item deleted successfully' });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
 
-// export const updateCartsAmount = async (req, t) => {
-//   return await carts.update(
-//     { amount: req.body.amount },
-//     {
-//       where: { id: req.params.id },
-//       transaction: t,
-//     },
-//   );
-// };
-
-// export const updateChecks = async (req, t) => {
-//   const result = await findOneCartById(req);
-//   if (result) {
-//     return await carts.update(
-//       { checked: req.body.checked },
-//       { where: { id: req.params.id }, transaction: t },
-//     );
-//   }
-// };
-// export const updateChecksAll = async (req, t) => {
-//   console.log(req.body);
-//   return await carts.update(
-//     { checked: req.body.checked },
-//     {
-//       where: {
-//         userId: req.tokenData.id,
-//         inventoryId: req.body.inventoryIdArray,
-//       },
-//       transaction: t,
-//     },
-//   );
-// };
-
-// export const findStoreIdFromUUID = async (req) => {
-//   return await stores.findOne({
-//     where: { UUID: req.query.UUID },
-//     raw: true,
-//     attributes: ['id', 'name'],
-//   });
-// };
-
-// export const getCarts = async (req, storeId) => {
-//   return await carts.findAll({
-//     where: { userId: req.tokenData.id },
-//     raw: true,
-//     nest: true,
-//     include: [
-//       {
-//         model: inventory,
-//         as: 'inventory',
-//         required: true,
-//         where: { storeId },
-//         attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
-//         include: [
-//           {
-//             model: discount,
-//             as: 'discounts',
-//             attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
-//           },
-//           {
-//             model: product,
-//             as: 'product',
-//             required: true,
-//             attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt'] },
-//             include: [
-//               {
-//                 model: productImage,
-//                 as: 'product_images',
-//                 required: true,
-//                 attributes: ['image', 'productId', 'id'],
-//               },
-//             ],
-//           },
-//         ],
-//       },
-//     ],
-//     attributes: { exclude: ['createdAt', 'updatedAt', 'deletedAt', 'userId'] },
-//   });
-// };
-
-// export const deleteOneProductInCart = async (req, t) =>
-//   await carts.destroy({
-//     where: { id: req.params.id },
-//     transaction: t,
-//   });
-
-// export const deleteCheckedItemInCart = async (req, t) =>
-//   await carts.destroy({
-//     where: { userId: req.tokenData.id, inventoryId: req.body.inventoryIdArray },
-//     transaction: t,
-//   });
-
-// export const deleteAllProductInCartSpecificStore = async (req, t) =>
-//   await carts.destroy({
-//     where: { userId: req.tokenData.id, storeId: req.body.storeId },
-//     transaction: t,
-//   });
-
-// export const getItemInCartDiscount = async (itemArray) => {
-//   return await discount.findAll({
-//     where: { inventoryId: itemArray },
-//     raw: true,
-//     nest:true,
-//     attributes: { exclude: ['createdAt', 'updatedAt'] },
-//   });
-// };
--- apps/api/src/routers/carts.router.js
+++ apps/api/src/routers/carts.router.js
@@ -1,23 +1,14 @@
 import { Router } from 'express';
-import { DB } from '../db';
 import {
-  addCarts,
   checkAllController,
-  deleteCheckedItemInCart,
+  deleteCheckedItemController,
   deleteOneCartController,
-  deleteOneProductInCart,
-  findOneCartById,
-  findOneCartByInventoryId,
+  deleteOneItemController,
   getCartsController,
-  incrementCartAmountBy1,
   postCartsController,
   updateCartController,
-  updateCartsAmount,
-  updateChecks,
-  updateChecksAll,
 } from '../controllers/carts.controller';
 import { validateToken, validateUser } from '../middleware/tokenValidation';
-import resTemplate from '../helper/resTemplate';
 const cartRouter = Router();
 //GET
 cartRouter.get('/', validateToken, validateUser, getCartsController);
@@ -28,39 +19,13 @@ cartRouter.post('/', validateToken, validateUser, postCartsController);
 
 cartRouter.patch('/checkall', validateToken, checkAllController);
 //Delete
-cartRouter.delete('/:id', validateToken,deleteOneCartController )
+cartRouter.delete('/:id', validateToken, deleteOneCartController);
 
 cartRouter.patch('/:id', validateToken, updateCartController);
 
 //Delete
 
-cartRouter.delete('/', validateToken, async (req, res, next) => {
-  await DB.initialize();
-  try {
-    await DB.db.sequelize.transaction(async (t) => {
-      await deleteCheckedItemInCart(req, t);
-    });
-    return res
-      .status(200)
-      .json({ success: true, message: 'checked items deleted successfully' });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+cartRouter.delete('/', validateToken, deleteCheckedItemController);
 
-cartRouter.delete('/delete/:id', validateToken, async (req, res, next) => {
-  await DB.initialize();
-  try {
-    await DB.db.sequelize.transaction(async (t) => {
-      await deleteOneProductInCart(req, t);
-    });
-    return res
-      .status(200)
-      .json({ success: true, message: 'Item deleted successfully' });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
+cartRouter.delete('/delete/:id', validateToken, deleteOneItemController);
 export { cartRouter };
