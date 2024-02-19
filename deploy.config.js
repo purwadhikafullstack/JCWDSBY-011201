5a6429b2eaa366c35f5d031951db1ec21f7dd0b5
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2024-02-19 13:23:36 +0700

    Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'JCWDSBY-0112-01', // Format JCWD-{batchcode}-{groupnumber}
-      script: './apps/api/src/index.js',
+      script: './apps/api/dist/index.js',
       env: {
         NODE_ENV: 'production',
         PORT: 1121,
